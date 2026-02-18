6459c4f411af5be0f9e190bd5b37c06ebc79ac5d
Gabriel Cervantes <[Your GitHub Email Address]>
2026-02-18 11:40:32 -0500

    Completed hi function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const renderData = () => {
-  // redndering data
+    // redndering data
 }
 const grabTheFile = () => {
     //add this text to fetch the api-merge conflict
 };
+
+for (i = 0; i < 10; i++) {
+    console.log('hi')
+}
+//how many different hi's will there be?
